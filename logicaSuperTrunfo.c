2e923212e1f4a17d87574b927ce34ac83b235ef4
AmadeuHerFur <amadeuherfur@gmail.com>
2025-06-18 10:10:13 -0300

    Update logicaSuperTrunfo.c
    Estabelecida a captura de valores

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -2,18 +2,51 @@
 inicio do teste
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+
+//O primeiro bloco do código se refere ao Desafio Iniciante
+//Será utilizada a estrutura inicial do primeiro desafio
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
-
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+   \\Primeira Carta
+    
+   printf("Informações referentes ao estado A\n");
+   printf("Informe o Código para a primeira carta:\n");
+   scanf("%s" , &codigo1); 
+   printf("Informe uma cidade para esta Carta\n");
+   scanf("%s" , &cidade1);
+   printf("Informe o valor para a População\n");
+   scanf("%d" , &populacao1);
+   printf("Informe o valor para a Área\n");
+   scanf("%f" , &area1);
+   printf("Informe o valor para o PIB\n");
+   scanf("%f" , &pib1);
+   printf("Informe o valor para o Número de Pontos Turísticos1\n");
+   scanf ("%d" , &ponto1);
+   
+   float densidade1 = (float) populacao1 / area1;
+   float capita1 = (float) pib1 / populacao1;
+   float poder1 = (float) 1/densidade1 + populacao1 + area1+ pib1 + ponto1 + capita1 
+    
+   \\Segunda Carta 
+    
+   printf("Informações referentes ao estado B\n");
+   printf("Informe o Código a segunda carta:\n");
+   scanf("%s" , &codigo2);
+   printf("Informe uma cidade para esta Carta\n");
+   scanf(%s , &cidade2);
+   printf("Informe o valor para a População\n");
+   scanf(%d , &populacao2);
+   printf("Informe o valor para a Área\n");
+   scanf(%f , &area2);
+   printf("Informe o valor para o PIB\n");
+   scanf(%f , &pib2);
+   printf("Informe o valor para o Número de Pontos Turísticos1\n");
+   scanf (%d , &ponto2);
+
+   float densidade2 = (float) populacao2 / area2;
+   float capita2 = (float) pib2 / populacao2;
+   float poder2 = (float) 1/densidade2 + populacao2 + area2+ pib2 + ponto2 + capita2 
 
     // Exemplo:
     // printf("Digite o código da cidade: ");
